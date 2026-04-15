2c6e5d6a743c60b45ddfd4b44561179fdcc33320
Jan Vávra <honzav06@gmail.com>
2026-04-15 10:16:24 +0200

    App rename

--- VideoAnalyzer/Program.cs
+++ VideoAnalyzer/Program.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Spectre.Console;
 
-namespace ConsoleApp1
+namespace VideoAnalyzer
 {
     internal class Program
     {
